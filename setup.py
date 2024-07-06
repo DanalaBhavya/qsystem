3199f1b4bdab2f6550ece231dd4b6bbeaec6705e
DanalaBhavya <dhanalabhavya1555@gmail.com>
2024-07-06 20:10:08 +0530

    QA commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'Bhavya Danala',
+    author_email= 'bhavyadanala1804@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
